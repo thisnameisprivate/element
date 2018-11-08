efc5ae2f58670bccfd07441782aaab7e9e87d5f5
上层精灵的挽歌 <40999381+thisnameisprivate@users.noreply.github.com>
2018-11-08 23:30:34 +0800

    Update App/Admin/Controller/IndexController.class.php

--- App/Admin/Controller/IndexController.class.php
+++ App/Admin/Controller/IndexController.class.php
@@ -1,5 +1,18 @@
 <?php
 
+
+/* ****************************************************************************************************************
+ *                                                                                                               **
+ *  This system is used to monitor the status of hospital return visit information registration.                 **
+ *  real-time communication Echarts graph trend/distribution, and to monitor the situation of diagnosis.         **
+ *  The architecture USES Redis, MySQL, PHP, Swolle [MeepoPS ThinkPHP framework].                                **
+ *  Developer: KeXin; Creation time: 2018/10/27.                                                                 **
+ *  Acknowledgement: KeXin; Development cycle: February - no maintenance stop.                                   **
+ *  Maintenance staff: kexin                                                                                     **
+ *                                                                                                               **
+ * ****************************************************************************************************************
+ * ****************************************************************************************************************
+ * */
 namespace Admin\Controller;
 use Think\Controller;
 use Think\Exception;
