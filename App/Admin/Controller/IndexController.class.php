9e3ee370499201883e03499cb48ad397a9f108f2
thisnameisprivate <2632798785@qq.com>
2018-11-09 11:12:26 +0800

    save code.

--- App/Admin/Controller/IndexController.class.php
+++ App/Admin/Controller/IndexController.class.php
@@ -1041,7 +1041,7 @@ class IndexController extends Controller {
         try {
             $redis = new \Redis();
             $redis->connect('211.149.x.x', 6379);
-            $redis->auth('xxxxxx');
+            $redis->auth('xxxxxxx');
             $redis->select(1);
         } catch (Exception $e) {
             die ("Connect Redis Fail: " . $e->getMessage());
