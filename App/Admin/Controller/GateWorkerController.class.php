db19ae808227124b147b53f128e07eae3a08a878
上层精灵的挽歌 <40999381+thisnameisprivate@users.noreply.github.com>
2018-11-21 21:39:43 +0800

    add GatewayWorkerController.class.php, add GateWorkerModel.class.php

--- App/Admin/Controller/GateWorkerController.class.php
+++ App/Admin/Controller/GateWorkerController.class.php
@@ -5,6 +5,6 @@ use Think\Controller;
 
 class GateWorkerController extends Controller {
     public function worker ($client_id) {
-        if (! emtpy($client_id)) {}
+        D('GateWorker')->getClientId($client_id);
     }
 }
--- App/Admin/Model/GateWorkerModel.class.php
+++ App/Admin/Model/GateWorkerModel.class.php
@@ -2,3 +2,10 @@
 
 namespace Admin\Model;
 use Think\Model;
+use GatewayWorker\Lib\Gateway;
+
+class GateWorkerModel extends Model {
+    public function getClientId ($client_id) {
+        Gateway::$registerAddress = '211.149.x.x:1238';
+    }
+}
